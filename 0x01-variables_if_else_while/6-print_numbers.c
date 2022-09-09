fb035f21363c0d17846313a9c93d6886d4acf88a
bami98 <beamlaksamson4@gmail.com>
2022-09-09 16:39:08 -0700

    updated

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry block
+ * Description - prints digits 0-10
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar("\n");
+
+	return (0);
+}
