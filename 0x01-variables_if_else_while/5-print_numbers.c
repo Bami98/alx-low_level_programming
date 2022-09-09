2f2aa0ade7d602a3a9d88f306b954e68f60a4d9f
bami98 <beamlaksamson4@gmail.com>
2022-09-09 16:11:38 -0700

    prints 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry block
+ * Description - prints digits 0-10
+ * Return: 0
+ */
+int main(void)
+{
+	for (int n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}
