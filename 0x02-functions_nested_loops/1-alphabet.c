b407deadf3964d54cadb55d26e92a1ee4fda2693
= <=>
2022-09-14 16:15:42 -0700

    prints lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabet 
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
