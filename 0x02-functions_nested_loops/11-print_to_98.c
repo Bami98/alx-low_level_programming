53f6cfc43dd085c53ad84e93cdd2a1847d1b7b7d
= <=>
2022-09-15 06:54:04 -0700

    print n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,14 +6,27 @@
 void print_to_98(int n)
 {
 	int i;
-	
-	for (i = n; i <= 98; i++)
+
+	if (n <= 98)
 	{
-		_putchar(i + '0');
-		if ( i != 98)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 }
