8124d618f15e704bdcb7b5a5ea3817454fef405b
= <=>
2022-09-14 18:00:30 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,22 @@
 #include "main.h"
 /**
- * print_last_digit(int) - prints the last digit
+ * print_last_digit - prints the last digit
  * @n: the number entered
  * Return: @n's last digit
  */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int last_digit;
+
+	last_digit = n % 10;
+	if (last_digit > 0)
+	{
+		_putchar('0' + last_digit);
+		return (last_digit);
+	}
+	else
+	{
+		_putchar('0' + last_digit');
+		return (last_digit);
+	}
 }
