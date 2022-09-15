0b8bddfa31c493473a285e6284bf257a38017bc4
= <=>
2022-09-14 17:07:45 -0700

    .

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,14 @@
 /**
  * _islower - checks whether an input character is lowercase or uppercase
  * @c: the character to be checked
- * Return: 1 on success
- * Otherwise 0
+ * Return: 1 it it is lowercase, if not 0
  *
  */
 int _islower(int c)
 {
 	if (c >= 97 && c >= 122)
-	{
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 
 }
