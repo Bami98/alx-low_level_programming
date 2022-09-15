83ef4627a55207165a0b3773fa033903fc795612
= <=>
2022-09-15 05:36:14 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,19 +4,19 @@
  */
 void times_table(void)
 {
-	int i,j;
-	
-	for(i = 0; i < 10; i++)
+	int j,k;
+
+	for (j = 0; j <= 9; j++)
 	{
-		for(j = 0; j < 10; j++)
+		for (k = 0; k <=9; k++)
 		{
-			int k = 0;
-			k = i * j;
-			_putchar(k);
-			_putchar(',');
-			_putchar(' ');
+			int a = k * j;
+			_putchar(a + '0');
+			if (k != 9)
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 		}
 		_putchar('\n');
 	}
 }
-
