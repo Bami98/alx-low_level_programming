2a8a16b8ea582e97b4bb26dc08e753531d5b0a44
= <=>
2022-09-15 05:18:53 -0700

    print time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,38 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day ( 00:00 to 23:59)
- * putchar: prints a characer
+ * jack_bauer - prints every minute of the day (00:00 to 23:59)
  */
 void jack_bauer(void)
 {
-	int i = 0, j;
+	int second = 0;
 
-	while( i < 24)
+	int h = 0, m = 0, s = 0, d = 0;
+
+	while (second < 1440)
 	{
-		j = 0;
-		while( j < 60)
+		_putchar(h + '0');
+		_putchar(m + '0');
+		_putchar(':');
+		_putchar(s + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+
+		d++;
+		if (d > 9)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-		 	_putchar(':');
-          		_putchar((j / 10) + '0');
-			_putchar((j % 10 + '0');
-	         	_putchar('\n');
-			j++;
+			d = 0;
+			s++;
 		}
-		i++;
+		if (s > 5)
+		{
+			s = 0;
+			m++;
+		}
+		if (m > 9)
+		{
+			m = 0;
+			h++;
+		}
+		second++;
 	}
-}	
+}
