0f9d39eb6c3fd77b9da37bf535ec473aca5539f3
= <=>
2022-09-15 06:24:33 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - it adds two integers
+ * @n: input integer
+ * @b: input integer
+ * Return: result of the added number
+ */
+int add(int n, int b)
+{
+	return (n + b);
+}
