8cd51e6ec97e32cec60b4538acd82e34e70d8c09
bami98 <beamlaksamson4@gmail.com>
2022-09-16 12:37:35 -0700

    check for digit

---
+++ 0x04-more_functions_nested_loops/1-digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit
+ * @c: parameter to be checked
+ * Return: 1 if true and 0 if false
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
