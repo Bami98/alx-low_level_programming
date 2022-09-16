f52850aa1fd2024614b58cf731d50a45b7beb029
bami98 <beamlaksamson4@gmail.com>
2022-09-16 13:07:41 -0700

    exclude 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,16 +5,14 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
-
-	while (i < 10)
+	for (int i = 0; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
 		{
 			continue;
 		}
-		_putchar(i + '0');
-		i++;
+		_putchar('i');
 	}
 	_putchar('\n');
+	return (0);
 }
