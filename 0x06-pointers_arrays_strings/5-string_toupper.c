25611b1ad567d291d36ab2b8a6e14407af1d280c
bami98 <beamlaksamson4@gmail.com>
2022-09-22 16:58:04 -0700

    .

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_to upper;
+ * @str : string
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
