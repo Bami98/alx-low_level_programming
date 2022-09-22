c6207467335b702f2cec706d5c70ece8c6e3c7ae
bami98 <beamlaksamson4@gmail.com>
2022-09-22 15:05:44 -0700

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - returns a char pointer after it concatenates 2 strings
+ * dest: destination parameter
+ * src: src parameter
+ * Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j = -1;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	do {
+		j++;
+		dest[i] = src[j];
+		i++;
+	} while (src[j] != '\0');
+	
+	return (dest);
+}
