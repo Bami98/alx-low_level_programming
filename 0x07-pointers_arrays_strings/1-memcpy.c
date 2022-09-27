6ab278543f4148184517cb9b8b098df7781092c9
bami98 <beamlaksamson4@gmail.com>
2022-09-27 14:37:15 -0700

    add soln to task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,23 @@
+#include "main.h"
 /**
- *
- *
+ * _memcpy - copy memory area
+ * @dest: char array to copy into
+ * @src: char array to copy from
+ * @n: number of elements to copy
+ * Return: pointer to `dest`
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
-	
-	while (i < n)
+	char *s;
+
+	s = dest;
+	while (n > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
 	}
-	return (dest);
+	return (s);
 }
