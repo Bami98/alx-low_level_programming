4d45dc95b3b4e368f791aa1b299d72e50bec260b
bami98 <beamlaksamson4@gmail.com>
2022-09-27 15:09:06 -0700

    add solution to task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+/**
+ * _strchr - locates a character in a string
+ * @s: string s
+ * @c: character to be located
+ * Return: pointer to the occurrence of the char c or
+ *         NULL if the char is not found
+ */
+char *_strchr(char *s, char c)
+{
+	char *p = NULL;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			p = s;
+			break;
+		}
+		s++;
+	}
+	return (p);
+}
+
