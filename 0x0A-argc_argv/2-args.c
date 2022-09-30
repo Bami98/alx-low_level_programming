a81a246778c28308b87e10311b4d45cd44b1ca27
bami98 <beamlaksamson4@gmail.com>
2022-09-30 12:34:51 -0700

    add solution for task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints every argument that is passed
+ * @argc: no of argument
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+
