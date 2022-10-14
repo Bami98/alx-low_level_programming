acf13554f109b57ab0e7fc71e9c478bd14b4ad4a
Bami98 <111159390+Bami98@users.noreply.github.com>
2022-10-14 15:26:48 -0700

    add solution for task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 
 /**
  * array_iterator - calls a function pointer on each element of an array
- * @array: array of integers
+ * @array: array to iterate
  * @size: size of array
  * @action: function pointer
  */
@@ -11,6 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array != NULL && size > 0 && action != NULL)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
